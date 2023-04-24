028ac5ca62fbb59921f11db89f88ff1832947e4e
LucasAL11 <lucasluna1000@gmail.com>
2023-04-24 15:14:58 -0300

    [init]

---
+++ PasswordGenarator.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+public static class PasswordGenarator
+{
+    public static string GetRandomPassword(int length)
+    {
+        const string chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        var substring = new StringBuilder();
+        var random = new Random();
+
+        for (var i = 0; i < length; i++)
+        {
+            var index = random.Next(chars.Length);
+            substring.Append(chars[index]);
+        }
+
+        return substring.ToString();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Pressione qualquer tecla para gerar um senha");
+
+var password = PasswordGenarator.GetRandomPassword(12);
+
+Console.WriteLine(password);
